2cdf29c45c5de2a291890f60ef03557bd9117264
nathantouss <147151541+nathantouss@users.noreply.github.com>
2023-11-27 18:32:50 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'observations/new'
-  get 'observations/create'
-  get 'observations/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,4 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :observations, only: %i[index new create]
 end
