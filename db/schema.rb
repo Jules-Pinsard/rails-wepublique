07cb3f1d1538b67c67168717ec0f908862d218e4
nathan toussaint <nathan.toussaint42@gmail.com>
2023-11-27 14:39:38 +0100

    comment_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_130445) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_27_132254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "mesure_id", null: false
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mesure_id"], name: "index_comments_on_mesure_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "mesures", force: :cascade do |t|
     t.string "title"
@@ -39,5 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_130445) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "mesures"
+  add_foreign_key "comments", "users"
   add_foreign_key "mesures", "users"
 end
