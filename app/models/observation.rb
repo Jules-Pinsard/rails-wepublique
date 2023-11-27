23a492540d20ffd723f16ac8332e38afe3d43f60
Mael Lemire <mael.lemire@icloud.com>
2023-11-27 17:55:50 +0100

    controller et valids

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -2,4 +2,11 @@ class Observation < ApplicationRecord
   belongs_to :user
   belongs_to :category
   acts_as_votable
+
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :dangerosity, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates_presence_of :user
+  validates_presence_of :category
 end
