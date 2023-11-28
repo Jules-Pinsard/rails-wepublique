a454db3ffd241032385890099a52660fe3731c6e
nathan toussaint <nathan.toussaint42@gmail.com>
2023-11-28 15:42:24 +0100

    ajout_category_back

--- db/migrate/20231128102953_add_coordinates_to_observations.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToObservations < ActiveRecord::Migration[7.1]
-  def change
-    add_column :observations, :latitude, :float
-    add_column :observations, :longitude, :float
-  end
-end
