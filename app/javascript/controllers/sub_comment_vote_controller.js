897532c95ef766fd6a967df06ca003a6ce81232c
nathan toussaint <nathan.toussaint42@gmail.com>
2023-12-06 14:28:16 +0100

    login_modifs

--- app/javascript/controllers/sub_comment_vote_controller.js
+++ app/javascript/controllers/sub_comment_vote_controller.js
@@ -3,22 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="upvote"
 export default class extends Controller {
   static targets = ['subupvoteArea', 'subdownvoteArea']
+  static values = {user: Number}
 
   connect() {}
 
   vote(event) {
-    event.preventDefault();
-    fetch(event.currentTarget.href, {
-      method: 'PATCH',
-      headers: {
-        "Accept": 'application/json',
-        "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      this.subupvoteAreaTarget.outerHTML = data.sub_upvote_html;
-      this.subdownvoteAreaTarget.outerHTML = data.sub_downvote_html;
-    })
+    if (this.userValue === 0 ) {
+      return
+    } else {
+      event.preventDefault();
+      fetch(event.currentTarget.href, {
+        method: 'PATCH',
+        headers: {
+          "Accept": 'application/json',
+          "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.subupvoteAreaTarget.outerHTML = data.sub_upvote_html;
+        this.subdownvoteAreaTarget.outerHTML = data.sub_downvote_html;
+      })
+    }
   }
 }
